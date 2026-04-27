55712e24ab51319587e3683dd0231c58ce2bbd13
Uniswap Dev Team <dev@uniswap.local>
2026-04-27 13:35:21 +0800

    Drop firebase_storage dependency
    - Remove firebase_storage from pubspec
    - Strip uploadAvatar / uploadListingImage helpers from FirebaseService
    - Auth + Firestore remain wired

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 /// Centralized Firebase singletons used across the app.
 class FirebaseService {
@@ -10,7 +7,6 @@ class FirebaseService {
 
   static FirebaseAuth get auth => FirebaseAuth.instance;
   static FirebaseFirestore get db => FirebaseFirestore.instance;
-  static FirebaseStorage get storage => FirebaseStorage.instance;
 
   // Collections
   static CollectionReference<Map<String, dynamic>> get users =>
@@ -18,23 +14,4 @@ class FirebaseService {
 
   static CollectionReference<Map<String, dynamic>> get listings =>
       db.collection('listings');
-
-  /// Uploads [file] to `avatars/{uid}.jpg` and returns the download URL.
-  static Future<String> uploadAvatar(String uid, File file) async {
-    final ref = storage.ref('avatars/$uid.jpg');
-    await ref.putFile(file);
-    return ref.getDownloadURL();
-  }
-
-  /// Uploads a listing image to `listings/{listingId}/{fileName}` and
-  /// returns the download URL.
-  static Future<String> uploadListingImage(
-    String listingId,
-    File file, {
-    String fileName = 'cover.jpg',
-  }) async {
-    final ref = storage.ref('listings/$listingId/$fileName');
-    await ref.putFile(file);
-    return ref.getDownloadURL();
-  }
 }
